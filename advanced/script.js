65d133e04308e598edef5831f5f62c7aecabb8a8
mackilroy007 <mackilroy007@gmail.com>
2020-10-12 13:53:07 +0200

    final styling touches. Content is displayed differently on small vs medium devices with their own breakpoints.

--- advanced/script.js
+++ advanced/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     for (let i = 0; i < actors.length; i++){
         $(".navBar").append(`
-            <a class="nav-link actorLink" id="${i}"{href="#"> ${actors[i].name}</a>
+            <a class="nav-link actorLink text-info" id="${i}"{href="#"> ${actors[i].name}</a>
         `)
 
     };
@@ -20,11 +20,11 @@ $(document).ready(function(){
         // create card with the index from json (same index number)
         $(".actorCard").html(`
         <div class="col card">
-            <h5 class="card-title">${actors[index].name}</h5>
+            <h5 class="card-title text-info">${actors[index].name}</h5>
             <img src="${actors[index].image}" class="card-img-top w-50 rounded mx-auto d-block " alt="...">
             <div class="card-body">
-                <p class="card-text"> Born: ${actors[index].birthday}</p>
-                <a href="${actors[index].imdb}" class="btn btn-primary">IMDB</a>
+                <p class="card-text text-info"> Born: ${actors[index].birthday}</p>
+                <a href="${actors[index].imdb}" class="btn btn-warning bg-warning text-dark font-weight-bold">IMDB</a>
             </div>
         </div>
         `);
